7719441fced57431227cf0c1136dda3c8c143b2b
Joshua K <i814u2@gmail.com>
2016-11-14 22:12:29 -0600

    startup wget fix

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python ./pip-upgrade-all.py
-wget https://github.com/UniversalDevicesInc/Polyglot/raw/unstable-release/bin/`cat file.arch`
+wget https://github.com/UniversalDevicesInc/Polyglot/raw/unstable-release/bin/`cat file.arch` -O `cat file.arch`
 ./`cat file.arch` -v
